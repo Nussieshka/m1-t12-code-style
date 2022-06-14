a0c18ca56746e1614220c526f0500408c44d4831
Agniia <nussieshka@yandex.ru>
2022-06-14 23:26:50 +0300

    Code style пир-ревью
    Объявить константы PERCENT_RATE и CONST
    Вывести печать процентной ставки
    Заменить условные операторы на switch
    Привести переменные к lowCamelCase
    Привести объявление переменных к единому стилю
    Переименовать параметры метода calculateComplexPercent
    Убрать лишние строки

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,38 +1,46 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double a, double y, int d) {
-        double pay = a * Math.pow((1 + y / 12), 12 * d);
-        return random(pay, 2);
+    static final int CONST = 2;
+    static final double PERCENT_RATE = 0.6;
+
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return random(pay, CONST);
     }
 
     double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
-        return random(amount + amount * yearRate * depositPeriod, 2);
+        return random(amount + amount * yearRate * depositPeriod, CONST);
     }
 
     double random(double value, int places) {
-        double ScaLe = Math.pow(10, places);
-        return Math.round(value * ScaLe) / ScaLe;
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
     }
 
     void printMenu() {
         int period;
         int action;
-
+        int amount;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
-        int amount = scanner.nextInt();
-
+        amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:");
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double result = 0;
-
-        if (action == 1) {
-            result = calculateSimplePercent(amount, 0.06, period);
-        } else if (action == 2) {
-            result = calculateComplexPercent(amount, 0.06, period);
+        double result = 0.0;
+        System.out.println("Процентная ставка: " + PERCENT_RATE);
+        switch (action) {
+            case 1:
+                result = calculateSimplePercent(amount, PERCENT_RATE, period);
+                System.out.println(result);
+                break;
+            case 2:
+                result = calculateComplexPercent(amount, PERCENT_RATE, period);
+                break;
+            default:
+                break;
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + result);
     }
